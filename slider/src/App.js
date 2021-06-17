7d0b50c97800e6d624715f685f3f823219794790
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-06-17 23:45:40 +0900

    hook을 사용한 캐러샐 슬라이드 2가지 방법

---
+++ slider/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Carousel1 from './components/carousel_slider/Carousel_slider';
+import Carousel2 from './components/carousel_slider/Carousel_slider2';
+function App() {
+  return (
+    <>
+      <Carousel1></Carousel1>
+      <Carousel2></Carousel2>
+    </>
+  );
+}
+
+export default App;
